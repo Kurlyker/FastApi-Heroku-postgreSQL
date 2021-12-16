8eb0e51b014337a0930e7234da0154a59c20c3f8
Kurlyker <48961326+Kurlyker@users.noreply.github.com>
2021-12-16 05:53:55 +0200

    Update locust.py

--- locust.py
+++ locust.py
@@ -1,18 +1,32 @@
-from locust import HttpUser, TaskSet, task
-
+from locust import HttpLocust, TaskSet, task
+from locust import ResponseError
+import json
+  
+   
 class UserBehavior(TaskSet):
-
-    @task(1)
+   
+    def __init__(self, parent):
+        super(UserBehavior, self).__init__(parent)
+        self.token = ""
+        self.headers = {}
+   
+     @task(1)
     def lol_index(self):
         self.client.get("/")
    
     @task(1)
-    def lol_index(self):
-        self.client.get("/users")        
+    def get_users(self):
+        self.client.get("/users")    
 
+          
+        return json.loads(response._content)['access']
+   
+   
   
-
-class WebsiteUser(HttpUser):
+class WebsiteUser(HttpLocust):
+    # The task_set attribute should point
+    # to a TaskSet class which defines 
+    # the behaviour of the user
     task_set = UserBehavior
-    min_wait = 1000
-    max_wait = 2000
+    min_wait = 5000
+    max_wait = 9000

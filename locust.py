da12396ba0369dfc98b9e70ceeae018c04a28c43
Kurlyker <48961326+Kurlyker@users.noreply.github.com>
2021-12-16 05:06:48 +0200

    Update locust.py

--- locust.py
+++ locust.py
@@ -4,47 +4,13 @@ class UserBehavior(TaskSet):
 
     @task(1)
     def lol_index(self):
-        self.client.get("/lol")
+        self.client.get("/")
+   
+    @task(1)
+    def lol_index(self):
+        self.client.get("/users")        
 
-    @task(1)
-    def team_profile(self):
-        self.client.get("/lol/teams/420")
-    
-    @task(1)
-    def tournament(self):
-        self.client.get("/lol/tournaments/10")
-
-    @task(1)
-    def university(self):
-        self.client.get("/lol/universities/1553")
-
-    @task(1)
-    def university_index(self):
-        self.client.get("/lol/universities")
-
-    @task(1)
-    def user_profile(self):
-        self.client.get("/lol/users/666")
-    
-    @task(1)
-    def scrims(self):
-        self.client.get("/lol/scrims")
-
-    @task(1)
-    def schedule(self):
-        self.client.get("/lol/schedules")
-
-    @task(1)
-    def playoffs(self):
-        self.client.get("/lol/playoffs")
-
-    @task(1)
-    def standings(self):
-        self.client.get("/lol/standings")
-    
-    @task(1)
-    def tournament(self):
-        self.client.get("/lol/tournaments/10")
+  
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
